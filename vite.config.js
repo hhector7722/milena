986af6dab08553057259c75cc6939c09a9bf5c57
hhector7722 <hhector7722@gmail.com>
2026-02-03 14:13:19 +0100

    UX: Update PWA icons to icon.png

--- vite.config.js
+++ vite.config.js
@@ -21,12 +21,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'wat.png',
+            src: 'icon.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'wat.png',
+            src: 'icon.png',
             sizes: '512x512',
             type: 'image/png'
           }
